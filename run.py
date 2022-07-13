bd006b06857699fa1f17fef6bb57eb6441a0bea8
Meyaal <alsuhaili_32@hotmail.com>
2022-07-13 11:30:29 +0000

    create a wordlist and a function to make a random choice

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+# below is a function to make a random choice from the word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
