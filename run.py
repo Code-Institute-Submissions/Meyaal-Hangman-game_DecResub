7f4ae37e7837c46769c513b5597082aa68a7983d
Meyaal <alsuhaili_32@hotmail.com>
2022-07-13 11:59:46 +0000

    create a function for play

--- run.py
+++ run.py
@@ -6,3 +6,20 @@ from words import word_list
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
+# create a play function for users and a while loop
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    
+    
+    
+    
