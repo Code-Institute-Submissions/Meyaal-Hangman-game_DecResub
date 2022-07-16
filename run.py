8fc122e823def5a366feaad20a345a8ca858ab39
Meyaal <alsuhaili_32@hotmail.com>
2022-07-16 05:53:23 +0000

    create a new variable for alphabet and code for validate input

--- run.py
+++ run.py
@@ -35,12 +35,50 @@ welcome_user()
 # let the user knows how many letters
 
 
+import random
+from word import words
+from hangman_structure import hangman_graphics
+
+
+def welcome_user():
+    """
+    This function allows user to type
+    their name.
+    user cannot use numbers
+    username must have characters only
+    """
+    username = None
+
+    while True:
+        username = input('Enter your name\n')
+
+        if not username.isalpha():
+            print('Username must be alphabets only')
+            continue
+        else:
+            print('welcome '+username)
+            break
+
+
+print('Welcome to Hangman')
+welcome_user()
+
+# create some variables for the game
+# number of misstakes
+# number af letters guessed
+# use the structure to show how many tries left
+# let the user knows how many letters
+# choose random word from list
+# let the user knows how many letters
+
+
 number_mistakes = 0
 letters_guessed = []
 number_mistakes_allowed = len(hangman_graphics)
 word = random.choice(words)
 letters_word = list(word)
 wrong_letters = []
+alphabet = ('abcdefghijklmnopqrstuvwxyz')
 
 print()
 print('The word has {} letters'.format(len(letters_word)))
@@ -58,6 +96,8 @@ while number_mistakes < number_mistakes_allowed:
         print()
         print('You have already entered this letter, enter another one')
         letter_user = input('Enter a letter --> ')
+    if letter_user not in alphabet:
+        print(' Make sure you enter an alphabet not a number')
 
     if letter_user not in letters_word:
         number_mistakes += 1
@@ -85,7 +125,6 @@ while number_mistakes < number_mistakes_allowed:
         print()
         print('YOU WOOOON!!!')
         break
-    
 
 if number_mistakes == number_mistakes_allowed:
     print()
