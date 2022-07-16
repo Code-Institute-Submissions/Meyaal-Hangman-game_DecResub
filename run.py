cba97978e2c7b4853bcc78b845725d1750c74fa2
Meyaal <alsuhaili_32@hotmail.com>
2022-07-16 14:07:28 +0000

    remove duplicate function on line 38

--- run.py
+++ run.py
@@ -34,39 +34,6 @@ welcome_user()
 # choose random word from list
 # let the user knows how many letters
 
-
-def welcome_user():
-    """
-    This function allows user to type
-    their name.
-    user cannot use numbers
-    username must have characters only
-    """
-    username = None
-
-    while True:
-        username = input('Enter your name\n')
-
-        if not username.isalpha():
-            print('Username must be alphabets only')
-            continue
-        else:
-            print('welcome '+username)
-            break
-
-
-print('Welcome to Hangman')
-welcome_user()
-
-# create some variables for the game
-# number of misstakes
-# number af letters guessed
-# use the structure to show how many tries left
-# let the user knows how many letters
-# choose random word from list
-# let the user knows how many letters
-
-
 number_mistakes = 0
 letters_guessed = []
 number_mistakes_allowed = len(hangman_graphics)
