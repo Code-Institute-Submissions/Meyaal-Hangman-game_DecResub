f7f30c184a766b18bf89315f45c0abd3d30009b1
Meyaal <alsuhaili_32@hotmail.com>
2022-08-24 10:10:03 +0000

    create file with hangman graphics and add a function

--- hangman_structure.py
+++ hangman_structure.py
@@ -2,13 +2,53 @@
 """
 file with hangman structure
 """
-hangman_graphics = ['_',
-                    '__',
-                    '__\n |',
-                    '__\n |\n O',
-                    '__\n |\n O\n |',
-                    '__\n |\n O\n/|',
-                    '__\n |\n O\n/|\ ',
-                    '__\n |\n O\n/|\ \n/',
-                    '__\n |\n O\n/|\ \n/ \ '                
-                    ]
+
+
+def hangman_graphics(x):
+    """
+    function to print hangman when user enters a wrong input
+    There are total 6 trials to the user and after every incorrect input,
+    hangman will be displayed
+    """
+if x == 0:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|        ")
+        print("|        ")
+        print("|        ")
+        print("===+")
+    if x == 1:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|      O ")
+        print("|        ")
+        print("|        ")
+        print("===+")
+    if x == 2:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|      O ")
+        print("|      | ")
+        print("|        ")
+        print("===+")
+    if x == 3:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|      O ")
+        print("|     /| ")
+        print("|        ")
+        print("===+")
+    if x == 4:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|      O ")
+        print("|     /|\ ")
+        print("|        ")
+        print("===+")
+    if x == 5:
+        print("\n+====+ ")
+        print("|      | ")
+        print("|      O ")
+        print("|     /|\ ")
+        print("|     / \ ")
+        print("===+")
--- run.py
+++ run.py
@@ -37,13 +37,53 @@ incorrect_guess = []
 
 def correct_guess_list():
     """
-    it will print the correct letters in the
+    the function will print the correct letters in the
     correct_guess list
     """
     for letter in correct_guess:
         print(letter, end=" ")
     print()
 
+def letters_only():
+    """
+    this function will validate input from users
+    only letters and no character or numbers
+    """
+    while True:
+        user_input_letter = input("type a letter: \n").lower()
+        if not user_input_letter.isalpha():
+            print('Error, please select a letter')
+        else:
+            return user_input_letter 
 
+correct_guess_list()
+hangman_graphics(len(incorrect_guess))
 
+while True:
+    user_input = letters_only()
+    if user_input in random_word:
+        INDEX = 0
+        for i in random_word:
+            if i == user_input:
+                correct_guess[INDEX] = user_input
+            INDEX += 1
+        correct_guess_list()
 
+    else:
+        if user_input not in incorrect_guess:
+            incorrect_guess.append(user_input)
+            hangman_graphics(len(incorrect_guess))
+            print(f'sorry, letter {user_input} is not in the word')
+
+        else:
+            print("You already guessed it, please try again...")
+        print(incorrect_guess)
+
+    if len(incorrect_guess) > 5:
+        print("You lose, please try again")
+        print("correct word is ", random_word)
+        break
+
+    if "_" not in correct_guess:
+        print("Congratulations!!!, you have guessed the correct letter")
+        break
