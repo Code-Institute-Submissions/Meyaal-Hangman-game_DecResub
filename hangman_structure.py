5d5fb8cf6c546cb75ea6fa4df083ffb3f3354bd4
Meyaal <alsuhaili_32@hotmail.com>
2022-08-29 12:34:49 +0000

    add hangman structure

---
+++ hangman_structure.py
@@ -0,0 +1,15 @@
+"""
+file with hangman structure
+"""
+hangman_graphics = ['_',
+                    '__',
+                    '__\n |',
+                    '__\n |\n O',
+                    '__\n |\n O\n |',
+                    '__\n |\n O\n/|',
+                    '__\n |\n O\n/|\ ',
+                    '__\n |\n O\n/|\ \n/',
+                    '__\n |\n O\n/|\ \n/ \ '                
+                    ]
+
+
