02eccd18326a7c2c51234f3d18bebd323ce2d4d5
Meyaal <alsuhaili_32@hotmail.com>
2022-12-03 11:51:31 +0000

    create a new file for validate input

---
+++ validateinput.py
@@ -0,0 +1,22 @@
+import re
+
+class ValidateInput():
+    
+    
+    __pattern = "[a-zA-Z]$"
+
+    __ok = "OK"
+    __invalid = "Invalid code, please enter again:"
+
+    def __init__(self):
+        pass
+
+
+    def validate(self, value):
+        
+        if re.match(self.__pattern, value):
+            print(self.__ok)
+            return True
+        else:
+            print(self.__invalid)
+            return False
